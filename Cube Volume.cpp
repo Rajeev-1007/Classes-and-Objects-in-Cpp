1369ffc8f70877ff7266df25a08f4d03e26e9663
Rajeev Enugu <rrajeevenugu@gmail.com>
2025-09-04 08:50:39 +0530

    Create Cube Volume.cpp

---
+++ Cube Volume.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Cube {
+private:
+    float side;   
+
+public:
+    Cube(float s) {   
+        side = s;
+    }
+
+    float volumeInside() {   
+        return side * side * side;
+    }
+};
+
+float volumeOutside(float s) {
+    return s * s * s;
+}
+
+int main() {
+    float s;
+    cout << "Enter side of cube: ";
+    cin >> s;
+
+    Cube c(s);
+
+    cout << "Volume (method inside class) = " << c.volumeInside() << endl;
+    cout << "Volume (method outside class) = " << volumeOutside(s) << endl;
+
+    return 0;
+}
